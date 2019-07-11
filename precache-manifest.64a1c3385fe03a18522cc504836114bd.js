492412db7c66a07439e190b1a1052a5ed2c2e7a4
Shane Collins <shane@collinz.ca>
2019-07-10 20:50:59 -0400

    Updates

---
+++ precache-manifest.64a1c3385fe03a18522cc504836114bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2a8d304cd5cb0fe2cf5eded0090e18",
+    "url": "/R-sum-_Website/index.html"
+  },
+  {
+    "revision": "3df0ffa3d12f68793cb7",
+    "url": "/R-sum-_Website/static/css/main.afcf6203.chunk.css"
+  },
+  {
+    "revision": "0f8c8654aa651ac51abe",
+    "url": "/R-sum-_Website/static/js/2.c5842a09.chunk.js"
+  },
+  {
+    "revision": "3df0ffa3d12f68793cb7",
+    "url": "/R-sum-_Website/static/js/main.a47b5f8b.chunk.js"
+  },
+  {
+    "revision": "fcb746f2a5cafe7b043d",
+    "url": "/R-sum-_Website/static/js/runtime~main.76156b08.js"
+  },
+  {
+    "revision": "c7797a83852935880a1d8e3ae58f12a2",
+    "url": "/R-sum-_Website/static/media/balconies2.c7797a83.jpg"
+  }
+]);
